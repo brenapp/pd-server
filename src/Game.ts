30470e5bfc988f4d200b6178192273063154baac
Brendan McGuire <brendan@bren.app>
2020-04-29 03:56:53 -0400

    reassignHost

--- src/Game.ts
+++ src/Game.ts
@@ -6,13 +6,17 @@ import WebSocket from "ws";
 import { randomBytes } from "crypto";
 import Player from "./Player";
 
-type ClientMessagesGame = {};
+type ClientMessagesGame = {
+  scope: "game";
+  action: "set-guessing";
+  guessing: string;
+};
 
 export default class Game {
   code = randomBytes(2).toString("hex").toUpperCase();
   static instances = new Map<string, Game>();
 
-  players: Set<Player> = new Set<Player>();
+  players: Map<string, Player> = new Map<string, Player>();
 
   constructor() {
     Game.instances.set(this.code, this);
@@ -28,7 +32,7 @@ export default class Game {
       });
     }
 
-    this.players.add(player);
+    this.players.set(player.state.id, player);
     this.broadcastStates();
   }
 
@@ -44,19 +48,61 @@ export default class Game {
       });
     }
 
-    this.players.delete(player);
+    this.players.delete(player.state.id);
     this.broadcastStates();
   }
 
+  reassignHost() {
+    let oldHost: Player | null = null;
+
+    // Remove the host
+    for (const [id, player] of this.players) {
+      if (player.state.host) {
+        oldHost = player;
+        player.setState({ host: false });
+        break;
+      }
+    }
+
+    // Make the new one (first player who is active and wasn't the previous host)
+    for (const [id, player] of this.players) {
+      if (id != oldHost?.state.id && oldHost?.state.active) {
+        player.setState({ host: true });
+        break;
+      }
+    }
+  }
+
+  setGuessing(guessing: string) {
+    for (const [id, player] of this.players) {
+      if (id === guessing) {
+        player.setState({ guessing: true });
+      } else {
+        player.setState({ guessing: false });
+      }
+    }
+  }
+
+  handleMessage = (data: WebSocket.Data) => {
+    const message: ClientMessagesGame = JSON.parse(data.toString());
+    if (!message || message.scope !== "game") return;
+
+    switch (message.action) {
+      case "set-guessing":
+        this.setGuessing(message.guessing);
+        break;
+    }
+  };
+
   broadcast(data: { broacastType: string; [key: string]: any }) {
-    for (const player of this.players) {
+    for (const [id, player] of this.players) {
       player.socket?.send(JSON.stringify({ action: "broadcast", ...data }));
     }
   }
 
   // For whenever state changes
   broadcastStates() {
-    const states = [...this.players].map((player) => player.state);
+    const states = [...this.players].map(([id, player]) => player.state);
     this.broadcast({
       broacastType: "state-update",
       states,
